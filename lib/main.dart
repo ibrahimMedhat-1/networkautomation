09e12a1b3348cb581efe7602ce06015730d38027
ibrahimmedhat <doha.kroz0@gmail.com>
2024-03-25 00:40:27 +0200

    engineer login

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:networkautomation/1.engineer/layout/view/layout.dart';
-
+import 'package:networkautomation/shared/features/authentication/view/login_page.dart';
 
 import 'firebase_options.dart';
 
@@ -25,7 +24,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
       ),
-      home: const EngineerLayout(),
+      home: const LoginPage(),
     );
   }
 }
